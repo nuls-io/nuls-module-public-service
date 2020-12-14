cb644ec608d9fe64ea089dfd80dd95ac719b8d7e
zhouwei1985 <251556768@qq.com>
2020-12-14 14:34:32 +0800

    add currentProtocolVersion

--- src/main/java/io/nuls/api/task/GetGlobalInfoTask.java
+++ src/main/java/io/nuls/api/task/GetGlobalInfoTask.java
@@ -2,6 +2,10 @@ package io.nuls.api.task;
 
 import io.nuls.api.ApiContext;
 import io.nuls.api.analysis.WalletRpcHandler;
+import io.nuls.base.api.provider.ServiceManager;
+import io.nuls.base.api.provider.protocol.ProtocolProvider;
+import io.nuls.base.api.provider.protocol.facade.GetVersionReq;
+import io.nuls.base.api.provider.protocol.facade.VersionInfo;
 import io.nuls.core.basic.Result;
 
 import java.util.Map;
@@ -14,6 +18,8 @@ public class GetGlobalInfoTask implements Runnable {
         this.chainId = chainId;
     }
 
+    ProtocolProvider transferService = ServiceManager.get(ProtocolProvider.class);
+
     @Override
     public void run() {
         Result<Map<String, Object>> result = WalletRpcHandler.getBlockGlobalInfo(chainId);
@@ -24,10 +30,16 @@ public class GetGlobalInfoTask implements Runnable {
         ApiContext.localHeight = Long.parseLong(map.get("localHeight").toString());
         ApiContext.networkHeight = Long.parseLong(map.get("networkHeight").toString());
 
-        if(ApiContext.magicNumber == 0) {
+        if (ApiContext.magicNumber == 0) {
             result = WalletRpcHandler.getNetworkInfo(chainId);
             map = result.getData();
             ApiContext.magicNumber = Integer.parseInt(map.get("magicNumber").toString());
+        }
+
+        io.nuls.base.api.provider.Result<VersionInfo> res = transferService.getVersion(new GetVersionReq());
+        if (res.isSuccess()) {
+            VersionInfo info = res.getData();
+            ApiContext.localProtocolVersion = info.getLocalProtocolVersion();
         }
     }
 }
