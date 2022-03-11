c6fc1874d32199cfb3cf589c23d4818df71c7baf
lijun.zhou <zhouj.172@gmail.com>
2022-03-11 14:53:38 +0800

    设置SSL版本

--- src/main/java/io/nuls/api/rpc/rest/utils/BasePriceProvider.java
+++ src/main/java/io/nuls/api/rpc/rest/utils/BasePriceProvider.java
@@ -7,11 +7,25 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Map;
 
 /**
@@ -30,11 +44,19 @@ public abstract class BasePriceProvider implements PriceProvider {
     }
 
     public Map<String, Object> httpRequest(String url) {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-//                .setSSLHostnameVerifier((hostName, sslSession) -> {
-//            return true; // 证书校验通过
-//        })
-                .build();
+        SSLContext sslcontext = createIgnoreVerifySSL();
+// 设置协议http和https对应的处理socket链接工厂的对象
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", new SSLConnectionSocketFactory(sslcontext)).build();
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        HttpClients.custom().setConnectionManager(connManager);
+//        CloseableHttpClient httpClient = HttpClientBuilder.create()
+////                .setSSLHostnameVerifier((hostName, sslSession) -> {
+////            return true; // 证书校验通过
+////        })
+//                .build();
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connManager).build();
 
         HttpGet httpGet = new HttpGet(url);
 //        本地调试专用
@@ -58,11 +80,56 @@ public abstract class BasePriceProvider implements PriceProvider {
             return null;
         } catch (IOException e) {
             Log.warn("调用接口:{} 异常1, {}", url, e.getMessage());
+            e.printStackTrace();
             return null;
         } catch (Exception e) {
             Log.warn("调用接口:{} 异常2, {}", url, e.getMessage());
             return null;
         }
+    }
+
+    public static SSLContext createIgnoreVerifySSL() {
+        SSLContext sslContext = null;// 创建套接字对象
+        try {
+            sslContext = SSLContext.getInstance("TLSv1.2");//指定TLS版本
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        // 实现X509TrustManager接口，用于绕过验证
+        X509TrustManager trustManager = new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                                           String paramString) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] paramArrayOfX509Certificate,
+                                           String paramString) throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+        try {
+            sslContext.init(null, new TrustManager[] { trustManager }, null);//初始化sslContext对象
+        } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+        return sslContext;
+    }
+
+    public static void main(String[] args) {
+        BasePriceProvider priceProvider = new BasePriceProvider("") {
+            @Override
+            public BigDecimal queryPrice(String symbol) {
+                return null;
+            }
+        };
+
+        priceProvider.httpRequest("https://www.baidu.com/");
+
     }
 
 
