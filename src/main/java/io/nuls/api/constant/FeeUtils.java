b26fe2f5273ae17db4297eb323ddf5773f59e6ab
Nicholas-MacLeod <nijian1286@gmail.com>
2024-08-30 11:46:17 +0800

    set fee unit

--- src/main/java/io/nuls/api/constant/FeeUtils.java
+++ src/main/java/io/nuls/api/constant/FeeUtils.java
@@ -62,14 +62,14 @@ public class FeeUtils {
                     "        \"assetId\": \"2-201\",\n" +
                     "        \"symbol\": \"BTC\",\n" +
                     "        \"decimals\": 8,\n" +
-                    "        \"feePerKB\": \"10000\",\n" +
+                    "        \"feePerKB\": \"100\",\n" +
                     "        \"scFeeFoefficient\": \"0.0001\"\n" +
                     "    },\n" +
                     "    {\n" +
                     "        \"assetId\": \"2-202\",\n" +
                     "        \"symbol\": \"ETH\",\n" +
                     "        \"decimals\": 18,\n" +
-                    "        \"feePerKB\": \"100000000000000\",\n" +
+                    "        \"feePerKB\": \"20000000000000\",\n" +
                     "        \"scFeeFoefficient\": \"10000000\"\n" +
                     "    }\n" +
                     "]", HashMap.class);
