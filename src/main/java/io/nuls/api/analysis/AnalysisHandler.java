b2a6fc9b841e8f3acfff01e1e335287de671d99e
Nicholas-MacLeod <nijian1286@gmail.com>
2024-12-06 15:53:54 +0800

    add chain logo

--- src/main/java/io/nuls/api/analysis/AnalysisHandler.java
+++ src/main/java/io/nuls/api/analysis/AnalysisHandler.java
@@ -759,16 +759,12 @@ public class AnalysisHandler {
             contractInfo.setSymbol(tokenSymbol.toString());
             contractInfo.setOwners(new ArrayList<>());
         }
-        if (contractInfo.isNrc20()) {
-            try {
-                contractInfo.setTokenName(map.get("nrc20TokenName").toString());
-                contractInfo.setSymbol(map.get("nrc20TokenSymbol").toString());
-                contractInfo.setDecimals((Integer) map.get("decimals"));
-                contractInfo.setTotalSupply(map.get("totalSupply").toString());
-                contractInfo.setOwners(new ArrayList<>());
-            }catch (Exception e){
-                LoggerUtil.commonLog.error(e);
-            }
+        if (contractInfo.isNrc20()) {˚
+            contractInfo.setTokenName(map.get("nrc20TokenName").toString());
+            contractInfo.setSymbol(map.get("nrc20TokenSymbol").toString());
+            contractInfo.setDecimals((Integer) map.get("decimals"));
+            contractInfo.setTotalSupply(map.get("totalSupply").toString());
+            contractInfo.setOwners(new ArrayList<>());
         }
 
         List<Map<String, Object>> methodMap = (List<Map<String, Object>>) map.get("method");
